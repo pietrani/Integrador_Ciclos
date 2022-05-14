f2815ebbfe26db4c3149f88fa1974c881d4d69b1
pietrani <danielpietrani@gmail.com>
2022-05-14 10:14:05 -0300

    claseSiete

---
+++ src/While/PracticaWhile.java
@@ -0,0 +1,33 @@
+/*
+ Generar un ciclo while que cuente los pares del  0 al 100
+
+
+Generar otro while que carga los numeros del 0 al -100
+ */
+package While;
+
+/**
+ *
+ * @author CIRKUIT
+ */
+public class PracticaWhile {
+    
+    public static void main(String[] args) {
+        
+        
+       int contador =0; 
+        /*while (contador<=100){
+            
+            System.out.println(contador);
+            contador=contador+2 ;
+            
+            
+            
+        }*/
+        while (contador>=-100){
+          System.out.println(contador);  
+           contador--; 
+        }
+        
+    }
+}
